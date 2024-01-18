f42b2addbaef77f2f9e23b450fc9c57e12cd8a8d
peter <pngugi.wainaina@gmail.com>
2024-01-19 00:05:39 +0300

    badges lab done

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
+
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(speakers):
+    return ["Hello, my name is {}.".format(name) for name in speakers]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    return ["Hello, {}! You'll be assigned to room {}!".format(name, index + 1) for index, name in enumerate(speakers)]
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    rooms = assign_rooms(speakers)
+
+    for badge in badges:
+        print(badge)
+
+    for room_assignment in rooms:
+        print(room_assignment)
